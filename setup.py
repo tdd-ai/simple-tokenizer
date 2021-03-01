03de2db31edf51e5f9fb710167637f3fad8e25a3
alisafaya <alisafaya@gmail.com>
2021-03-01 20:46:35 +0300

    setup scripts

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name="simpletokenizer",
-    author="Ali Safaya",
-    packages = ['simpletokenizer'],
-    version='0.1',
-    author_email="alisafaya@gmail.com",
-    long_description=open("README.md", "r", encoding='utf-8').read(),
-    long_description_content_type="text/markdown",
-    download_url='https://github.com/tdd-ai/simple-tokenizer/archive/v0.1.tar.gz',
-    install_requires=[],
-    classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
-)
+setuptools.setup()
